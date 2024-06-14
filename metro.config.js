7ab5ba6775742d3b8b82b4c300789911e33b6e8e
Ramon Nunes <ramontsdev@gmail.com>
2024-06-14 09:00:51 -0300

    chore: eslint

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,8 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
 /**
  * Metro configuration
@@ -7,26 +10,15 @@ const { withNativeWind } = require('nativewind/metro');
  *
  * @type {import('metro-config').MetroConfig}
  */
-/* const config = mergeConfig(getDefaultConfig(__dirname), {
-  // your config
+
+const config = mergeConfig(defaultConfig, {
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer")
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"]
+  }
 });
 
-module.exports = withNativeWind(config, { input: './src/styles/global.css' }); */
-
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
-
-  const { transformer, resolver } = config;
-
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg'],
-  };
-
-  return withNativeWind(config, { input: './src/styles/global.css' });
-})();
+module.exports = withNativeWind(config, { input: './src/styles/global.css' });
